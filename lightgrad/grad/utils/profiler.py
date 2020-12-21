a02596c5dea9e7cdc762d5ed7d3e83547d5128c4
Niclas Doll <niclas@amazonis.net>
2020-12-21 19:03:48 +0100

    Reworked Profiler

--- lightgrad/grad/utils/profiler.py
+++ lightgrad/grad/utils/profiler.py
@@ -2,45 +2,35 @@ from time import time
 from functools import wraps
 from collections import defaultdict
 
-class __ProfilerMeta(type):
-    def __enter__(cls, *args):
-        cls._active = True
-    def __exit__(cls, *args):
-        cls._active = False
-class Profiler(object, metaclass=__ProfilerMeta):
-    # is profiler active
-    _active = False
-    __ft, __fc = defaultdict(float), defaultdict(int)
-    __bt, __bc = defaultdict(float), defaultdict(int)
-    class __Tracker(object):
-        def __init__(self, name, backward):
-            self.update = lambda td: Profiler.update(name, td, backward)
-        def __enter__(self, *args):
-            self.st = time()
-        def __exit__(self, *args):
-            self.update(time() - self.st)
-    @staticmethod
-    def update(name, time_delta, backward=False):
-        if Profiler._active:
-            (Profiler.__bt if backward else Profiler.__ft)[name] += time_delta
-            (Profiler.__bc if backward else Profiler.__fc)[name] += 1
-    @staticmethod
-    def profile(name, backward=False):
-        return Profiler.__Tracker(name, backward=backward)
-    @staticmethod
-    def print(topn=-1):
-        names = set(list(Profiler.__ft.keys()) + list(Profiler.__bt.keys()))
-        names = sorted(names, key=lambda n: -Profiler.__ft[n])
+class Profiler(object):
+    _active_profilers = []
+    def __init__(self):
+        self.__ft, self.__fc = defaultdict(float), defaultdict(int)
+        self.__bt, self.__bc = defaultdict(float), defaultdict(int)
+    def update(self, name, time_delta, backward=False):
+        (self.__bt if backward else self.__ft)[name] += time_delta
+        (self.__bc if backward else self.__fc)[name] += 1
+
+    def __enter__(self, *args):
+        Profiler._active_profilers.append(self)
+        return self
+    def __exit__(self, *args):
+        Profiler._active_profilers.remove(self)
+
+    def print(self, topn=-1):
+        names = set.union(set(self.__ft.keys()), set(self.__bt.keys()))
+        names = sorted(names, key=lambda n: -self.__ft[n])
         names = names[:topn] if topn > 0 else names
-        print(" Function       |   forward      \t|   backward   ")
-        print("-"*70)
+        print(" Function       |   forward      \t|   backward   \n" + "-"*70)
         for n in names:
             print(" %-15s| %8.4fs (%i)\t| %8.4fs (%i) " % (
-                n, Profiler.__ft[n], Profiler.__fc[n], Profiler.__bt[n], Profiler.__bc[n]))
+                n, self.__ft[n], self.__fc[n], self.__bt[n], self.__bc[n]))
         print("\n")
-    @staticmethod
-    def reset():
-        Profiler.__ft.clear()
-        Profiler.__fc.clear()
-        Profiler.__bt.clear()
-        Profiler.__bc.clear()
+
+class Tracker(object):
+    def __init__(self, name:str, backward:bool =False):
+        self.update = lambda td: [p.update(name, td, backward) for p in Profiler._active_profilers]
+    def __enter__(self, *args):
+        self.st = time()
+    def __exit__(self, *args):
+        self.update(time() - self.st)

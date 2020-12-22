b62d66a3c2656548b20b250c0b0be0f0a4134b72
Niclas Doll <niclas@amazonis.net>
2020-12-22 02:51:50 +0100

    loading pytorch state dict

--- lightgrad/nn.py
+++ lightgrad/nn.py
@@ -1,5 +1,5 @@
 import numpy as np
-from lightgrad.autograd import Tensor
+from lightgrad.autograd import Tensor, AbstractTensor
 
 class Module(object):
     def __init__(self):
@@ -12,11 +12,22 @@ class Module(object):
         return self.forward(*args, **kwargs)
 
     def __setattr__(self, name, val):
-        if isinstance(val, Tensor):
-            self.__paramters[name] = val
-        if isinstance(val, Module):
-            self.__modules[name] = val
+        if isinstance(val, (AbstractTensor, Module)):
+            self.register_param_or_module(name, val)
         object.__setattr__(self, name, val)
+
+    def register_param_or_module(self, name, val):
+        if isinstance(val, AbstractTensor):
+            self.__paramters[name] = val
+        elif isinstance(val, Module):
+            self.__modules[name] = val
+        return val
+    def unregister_param_or_module(self, name):
+        if name in self.__paramters:
+            return self.__paramters.pop(name)
+        if name in self.__modules:
+            return self.__modules.pop(name)
+
     def parameters(self) -> iter:
         # yield my parameters
         yield from iter(self.__paramters.values())
@@ -28,20 +39,63 @@ class Module(object):
         # map parameters
         for key, tensor in self.__paramters.items():
             tensor = fn(tensor)
-            self.__paramters[key] = tensor
-            setattr(self, key, tensor)
+            self.__setattr__(key, tensor)
         # map sub-modules
         for m in self.__modules.values():
             m.map_params(fn)
         return self
 
+    def load_params(self, param_dict:dict, prefix:str ="", separator:str ='.'):
+        if len(prefix) > 0:
+            prefix += separator
+        # load all my parameters
+        for key, p in self.__paramters.items():
+            # find parameter in dict
+            assert (prefix + key) in param_dict, "%s not found in param dict!" % (prefix + key)
+            new_p = param_dict[prefix + key]
+            # load in parameter
+            if not isinstance(new_p, p.__class__):
+                new_p = new_p.numpy() if isinstance(new_p, AbstractTensor) else new_p
+                assert isinstance(new_p, np.ndarray), "Unexpected parameter type %s!" % new_p.__class__.__name__
+                new_p = p.__class__.from_numpy(new_p)
+            assert p.shape == new_p.shape, "Shapes do not align! (%s != %s)" % (p.shape, new_p.shape)
+            self.__setattr__(key, new_p)
+        # load sub-module parameters
+        for key, m in self.__modules.items():
+            m.load_params(param_dict, prefix=prefix + key, separator=separator)
+
+class ModuleList(Module, list):
+    def __init__(self, *elements):
+        Module.__init__(self)
+        list.__init__(self, elements)
+        for i, e in enumerate(elements):
+            self.register_param_or_module(str(i), e)
+    def __setitem__(self, i, e):
+        assert i < len(self)
+        self.unregister_param_or_module(str(i))
+        self.register_param_or_module(str(i), e)
+        return list.__setitem__(self, i, e)
+    
 class Linear(Module):
     def __init__(self, in_feats:int, out_feats:int, bias:bool =True):
         Module.__init__(self)
-        self.w = Tensor.xavier((in_feats, out_feats))
-        self.b = Tensor.xavier((1, out_feats)) if bias else None
+        self.weight = Tensor.xavier((in_feats, out_feats))
+        self.bias = Tensor.xavier((out_feats,)) if bias else None
     def forward(self, x):
-        return (x @ self.w + self.b) if self.b is not None else (x @ self.w)
+        return (x @ self.weight + self.bias) if self.bias is not None else (x @ self.weight)
+    def load_params(self, param_dict:dict, prefix:str, separator:str):
+        # pytorch uses transposed weights - we dont :(
+        w_key = (prefix + separator + 'weight') if len(prefix) > 0 else 'weight'
+        assert w_key in param_dict, "%s not found in param dict!" % (w_key)
+        weight = param_dict[w_key]
+        # check if is transposed
+        is_transposed = (weight.shape[0] == self.weight.shape[1]) and (weight.shape[1] == self.weight.shape[0])
+        self.weight = self.weight.transpose(1, 0) if is_transposed else self.weight
+        # load parameters in and reverse transposition if necessary
+        Module.load_params(self, param_dict, prefix=prefix, separator=separator)
+        if is_transposed:
+            self.weight = self.weight.transpose(1, 0)
+            self.weight = self.weight.contiguous() if hasattr(self.weight, 'contiguous') else self.weight
 
 class Conv2d(Module):
     def __init__(self, in_channels:int, out_channels:int, kernelsize:int =3, stride:int =1, pad:int =None, bias:bool =True):
--- lightgrad/utils.py
+++ lightgrad/utils.py
@@ -12,3 +12,57 @@ def fetch(url):
             f.write(dat)
         os.rename(fp+".tmp", fp)
     return dat
+
+def load_torch_state_dict(bytes_):
+    import numpy as np
+    import io, pickle, struct
+
+    # convert it to a file
+    fb0 = io.BytesIO(bytes_)
+    # skip three junk pickles
+    pickle.load(fb0)
+    pickle.load(fb0)
+    pickle.load(fb0)
+
+    # pytorch data-storages
+    torch_storages = {
+        'FloatStorage': np.float32,
+        'LongStorage': np.int64
+    }
+
+    key_prelookup = {}
+    class TorchTensor:
+        def __new__(cls, *args):
+            ident, storage_type, obj_key, location, obj_size, view_metadata = args[0]
+            assert ident == 'storage'
+            tensor = np.zeros(obj_size, dtype=storage_type)
+            key_prelookup[obj_key] = (storage_type, obj_size, tensor, args[2], args[3])
+            return tensor
+
+    class TorchUnpickler(pickle.Unpickler):
+        def find_class(self, module, name):
+            # handle torch
+            if 'torch' in module:
+                return torch_storages.get(name, TorchTensor)
+            # fallback to default
+            return pickle.Unpickler.find_class(self, module, name)
+        def persistent_load(self, pid):
+            return pid
+
+    ret = TorchUnpickler(fb0).load()
+    # create key_lookup
+    key_lookup = pickle.load(fb0)
+    key_real = [None] * len(key_lookup)
+    for k,v in key_prelookup.items():
+        key_real[key_lookup.index(k)] = v
+    # read in the actual data
+    for storage_type, obj_size, np_array, np_shape, np_strides in key_real:
+        ll = struct.unpack("Q", fb0.read(8))[0]
+        assert ll == obj_size
+        bytes_size = {np.float32: 4, np.int64: 8}[storage_type]
+        mydat = fb0.read(ll * bytes_size)
+        np_array[:] = np.frombuffer(mydat, storage_type)
+        np_array.shape = np_shape
+        real_strides = tuple([x*bytes_size for x in np_strides])
+        np_array.strides = real_strides
+    return ret

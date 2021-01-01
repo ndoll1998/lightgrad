aa752d03828a1c4fb933e30bea3735216f1b9569
Niclas Doll <niclas@amazonis.net>
2021-01-01 02:40:42 +0100

    Forgot to rename

--- lightgrad/loss.py
+++ lightgrad/loss.py
@@ -15,10 +15,10 @@ class cross_entropy(Function):
     """ Cross Entropy Loss """
     def forward(ctx, y, y_hat, axis:int =-1):
         y = y.softmax(axis=axis)
-        ctx.save_for_backward(y, y_hat, dim)
+        ctx.save_for_backward(y, y_hat, axis)
         return -y[range(y_hat.shape[0]), y_hat].log().mean()
     def backward(ctx, out_grad):
-        y, y_hat, dim = ctx.get_saved_tensors()
+        y, y_hat, axis = ctx.get_saved_tensors()
         y[range(y_hat.shape[0]), y_hat] -= 1
         y /= y_hat.shape[0]        
         return y * out_grad
